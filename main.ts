164dd694255c69448ec42f8a7bf9bb24befd3fca
ZhangDing <akl7777777@163.com>
2024-03-11 18:45:49 +0800

    init

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow(): void {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('https://www.example.com');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
